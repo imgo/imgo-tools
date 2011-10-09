0efdf2f1790225b34fb99123874b5f12fcae2dc6
Alexander Boythenko <banzalik@gmail.com>
2011-10-09 20:45:25 +0300

    first commit

---
+++ Formula/imgo.rb
@@ -0,0 +1,24 @@
+require 'formula'
+
+class Imgo <Formula
+  url 'git://github.com/imgo/imgo.git'
+  homepage 'http://imgcomp.com'
+  md5 ''
+  @version='0.0.7'
+
+ depends_on 'imagemagick'
+ depends_on 'pngout'
+ depends_on 'optipng'
+ depends_on 'pngrewrite'
+ depends_on 'exiftool'
+ depends_on 'advancecomp'
+ depends_on 'jpeg'
+ depends_on 'gifsicle'
+ depends_on 'pngnq'
+ depends_on 'defluff'
+ depends_on 'cryopng'
+
+  def install
+    bin.install 'imgo'
+  end
+end

aaa752972f34de399434590f5b810505dc3352ea
isqua <isqua@isqua.ru>
2018-05-16 20:19:21 +0300

    Replace sha1 and md5 checksums with sha256

--- Formula/cryopng.rb
+++ Formula/cryopng.rb
@@ -3,8 +3,7 @@ require 'formula'
 class Cryopng <Formula
   url 'http://frdx.free.fr/cryopng/cryopng-macosx-ub.tgz'
   homepage 'http://frdx.free.fr/cryopng/'
-  md5 'b01942b43f7c098aaf585e3b1b72a852'
-  sha1 'dfd72c1ad81e02441591d438ea0782b54fa12fae'
+  sha256 'fd2981ba1c0e08018623dbc355554716ac814a1519ca5b91679260f0ef967a34'
   @version='0.6.4'
 
   def install
